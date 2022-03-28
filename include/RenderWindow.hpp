d422381686485f4b53c01c02f0a7eb85cc41e87f
daftylooper <89779139+daftylooper@users.noreply.github.com>
2022-03-28 20:19:36 +0530

    Add files via upload

---
+++ include/RenderWindow.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<vector>
+
+class RenderWindow{
+public:
+	RenderWindow(const char* p_title, int p_w, int p_h);
+	SDL_Texture* loadTexture(const char* p_filePath);
+	void cleanUp();
+	void clear();
+	void render(std::vector<std::vector<double>> vals);
+	void display();
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
